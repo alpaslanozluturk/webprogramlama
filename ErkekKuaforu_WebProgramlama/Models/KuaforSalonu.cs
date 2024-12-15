98f238bc857ff4d6bb2a163e1aa103dee0b23f12
alpaslanozluturk <alpaslan.ozluturk.business@gmail.com>
2024-12-15 09:01:14 +0300

    Kuaför Modelleri Oluşturuldu
    Kuaför modelleri oluşturuldu

---
+++ ErkekKuaforu_WebProgramlama/Models/KuaforSalonu.cs
@@ -0,0 +1,11 @@
+﻿namespace ErkekKuaforu_WebProgramlama.Models
+{
+    public class KuaforSalonu
+    {
+        public int Id { get; set; }
+        public string KuaforIsmi { get; set; }
+        public string Adresi { get; set; }
+        public string TelefonNumarasi { get; set; }
+
+    }
+}
