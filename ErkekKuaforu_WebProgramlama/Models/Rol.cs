fd41620d9b8902a4484f3c96dcde5a3f396c5f8d
aberkecindi <159448339+aberkecindi@users.noreply.github.com>
2024-12-15 22:51:50 +0300

    Identity kütüphaneleri, context, Identity sınıfları

---
+++ ErkekKuaforu_WebProgramlama/Models/Rol.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ErkekKuaforu_WebProgramlama.Models
+{
+    public class Rol : IdentityRole<string>
+    {
+    }
+}
---
+++ ErkekKuaforu_WebProgramlama/Veritabani/VeritabaniContext.cs
@@ -0,0 +1,18 @@
+﻿using ErkekKuaforu_WebProgramlama.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace ErkekKuaforu_WebProgramlama.Veritabani
+{
+    public class VeritabaniContext : IdentityDbContext<Kisi, Rol, string>
+    {
+        public VeritabaniContext(DbContextOptions<VeritabaniContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
