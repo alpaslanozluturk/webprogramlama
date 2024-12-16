46bda685365d717c0e2ce900e5ba6b2ae405f35b
alpaslanozluturk <alpaslan.ozluturk.business@gmail.com>
2024-12-16 19:15:21 +0300

    Kimlik doğrulama

---
+++ ErkekKuaforu_WebProgramlama/Controllers/KimlikController.cs
@@ -0,0 +1,43 @@
+﻿using ErkekKuaforu_WebProgramlama.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErkekKuaforu_WebProgramlama.Controllers
+{
+    public class KimlikController : Controller
+    {
+        private readonly UserManager<Kisi> _userManager;
+        private readonly SignInManager<Kisi> _signInManager;
+
+        public KimlikController(UserManager<Kisi> userManager, SignInManager<Kisi> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Giris(GirisModel girisModel)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Kayit(KayitModel kayitModel)
+        {
+            return View();
+        }
+        public IActionResult Cikis()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErkekKuaforu_WebProgramlama/Models/GirisModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ErkekKuaforu_WebProgramlama.Models
+{
+    public class GirisModel
+    {
+        [EmailAddress(ErrorMessage = "Email adresi girmelisiniz.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Şifrenizi giriniz.")]
+        [Display(Name = "Şifre")]
+        public string Sifre { get; set; }
+    }
+}
