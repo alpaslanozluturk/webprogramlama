b3751cceaf47f6aed856e4a6e2e6bb1b3c258edf
alpaslanozluturk <alpaslan.ozluturk.business@gmail.com>
2024-12-19 17:49:35 +0300

    Kimlik metodları dolduruldu, kuafor paneli eklendi

--- ErkekKuaforu_WebProgramlama/Controllers/KimlikController.cs
+++ ErkekKuaforu_WebProgramlama/Controllers/KimlikController.cs
@@ -21,8 +21,28 @@ namespace ErkekKuaforu_WebProgramlama.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Giris(GirisModel girisModel)
+        public async Task<IActionResult> Giris(GirisModel kisi)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(kisi);
+            }
+            var hasUser = await _userManager.FindByEmailAsync(kisi.Email);
+            if (hasUser == null)
+            {
+                ModelState.AddModelError(string.Empty, "Email ya da şifre yanlış!");
+                return View(kisi);
+            }
+            var result = await _signInManager.PasswordSignInAsync(hasUser.UserName!, kisi.Sifre, isPersistent: true, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, $"Kalan deneme hakkınız: {await _userManager.GetAccessFailedCountAsync(hasUser)}/4");
+            }
+
             return View();
         }
         [HttpGet]
@@ -31,9 +51,29 @@ namespace ErkekKuaforu_WebProgramlama.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Kayit(KayitModel kayitModel)
+        public async Task<IActionResult> Kayit(KayitModel kisi)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(kisi);
+            }
+            var kullanici = new Kisi()
+            {
+                UserName = kisi.Email,
+                Email = kisi.Email,
+                Isim = kisi.Isim,
+                Soyisim = kisi.Soyisim
+            };
+            var identityResult = await _userManager.CreateAsync(kullanici, kisi.SifreDogrulama);
+            if (!identityResult.Succeeded)
+            {
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+                return View(kisi);
+            }
+            return RedirectToAction(nameof(Giris), "Kimlik");
         }
         public IActionResult Cikis()
         {
